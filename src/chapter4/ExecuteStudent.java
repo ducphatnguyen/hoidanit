21f533b78c194737089c144b35f06b131ce2900b
Đức <duc894580@gmail.com>
2024-03-14 21:47:42 +0700

    Access Modifier

--- src/chapter4/ExecuteStudent.java
+++ src/chapter4/ExecuteStudent.java
@@ -5,14 +5,21 @@ public class ExecuteStudent {
         System.out.println("Run Student");
 
 //      Khai báo 1 object và gán đối tượng
-        Student st1 = new Student();
-        st1.name = "Duc";
-        st1.age = 25;
-        System.out.println("Student with name = " + st1.name + " and age = " + st1.age);
+//        Student st1 = new Student();
+//        st1.name = "Duc";
+//        st1.age = 25;
+//        System.out.println("Student with name = " + st1.name + " and age = " + st1.age);
+//
+//        Student st2 = new Student();
+//        st2.name = "Alex";
+//        st2.age = 23;
+//        System.out.println("Student with name = " + st2.name + " and age = " + st2.age);
 
-        Student st2 = new Student();
-        st2.name = "Alex";
-        st2.age = 23;
-        System.out.println("Student with name = " + st2.name + " and age = " + st2.age);
+        Student st1 = new Student();
+
+        Student st2 = new Student("Duc", 26);
+        System.out.println("Check object: " + st2.age + ": " + st2.name);
+
+
     }
 }
--- src/chapter4/Student.java
+++ src/chapter4/Student.java
@@ -5,7 +5,19 @@ public class Student {
     String name;
     int age;
 
+//  PTXD không đối số
+    public Student() {
+
+    }
+
+//  PTXD có đối số
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public void learnJava(){
         System.out.println("Learn Java with Youtube Hỏi Dân IT");
     }
+
 }
