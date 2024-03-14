e6100a1a4f02d6087fd375704b0b98f389cfd83e
Đức <duc894580@gmail.com>
2024-03-14 21:09:15 +0700

    Code with OOP

---
+++ src/chapter4/ExecuteStudent.java
@@ -0,0 +1,18 @@
+package chapter4;
+
+public class ExecuteStudent {
+    public static void main(String[] args) {
+        System.out.println("Run Student");
+
+//      Khai báo 1 object và gán đối tượng
+        Student st1 = new Student();
+        st1.name = "Duc";
+        st1.age = 25;
+        System.out.println("Student with name = " + st1.name + " and age = " + st1.age);
+
+        Student st2 = new Student();
+        st2.name = "Alex";
+        st2.age = 23;
+        System.out.println("Student with name = " + st2.name + " and age = " + st2.age);
+    }
+}
